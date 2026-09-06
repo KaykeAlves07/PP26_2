f176df68f3c20754ac6131231d4ab2abe7fe6833
Kayke Henrique dos Santos Alves <santoskayke451@gmail.com>
2026-09-06 10:27:38 -0300

    Atualiza arquivos da Calculadora 2

--- Calculadora2/Alvo.java
+++
@@ -1,7 +0,0 @@
-public interface Alvo {
-
-    public double soma(double ... numeros);
-    public double sub(double valorInicial, double ... numeros);
-    public double mult(double ... numeros);
-    public double divisao(double valorInicial, double ... numeros);
-}
